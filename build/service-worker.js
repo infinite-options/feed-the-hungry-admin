ac4779bd0e3aa2f37a404f8e4859ba2cce6f1af6
Aditya Agrawal <aditya.agrawal@sjsu.edu>
2020-06-14 16:26:02 -0700

    readme

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/feed-the-hungry-admin/precache-manifest.e3aca01d73abd1bf99868dbc6bd7af0b.js"
+  "/feed-the-hungry-admin/precache-manifest.1d01020cb68d87432833f7d289183172.js"
 );
 
 self.addEventListener('message', (event) => {
