044cedfdff98cca83a4d38ce00adc51455c8d4ed
Zach Lind <zlind@ucsc.edu>
2020-06-25 15:18:34 -0700

    built and deployed to github pages

---
+++ build/precache-manifest.9739d32cc8cda53c4688b17c376c3df6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20132c178102c2f3d7ec0cbcf5f4fb8",
+    "url": "/feed-the-hungry-admin/index.html"
+  },
+  {
+    "revision": "56c4f02c3d048cec4a7e",
+    "url": "/feed-the-hungry-admin/static/css/2.daf8c292.chunk.css"
+  },
+  {
+    "revision": "63b94b0ebba972ad2baa",
+    "url": "/feed-the-hungry-admin/static/css/main.25828032.chunk.css"
+  },
+  {
+    "revision": "56c4f02c3d048cec4a7e",
+    "url": "/feed-the-hungry-admin/static/js/2.15221279.chunk.js"
+  },
+  {
+    "revision": "9a7b27705511c83ffc42f2b448356ce4",
+    "url": "/feed-the-hungry-admin/static/js/2.15221279.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63b94b0ebba972ad2baa",
+    "url": "/feed-the-hungry-admin/static/js/main.fa3da6ff.chunk.js"
+  },
+  {
+    "revision": "bc109f37e08d132ae832",
+    "url": "/feed-the-hungry-admin/static/js/runtime-main.4524e3c1.js"
+  }
+]);
